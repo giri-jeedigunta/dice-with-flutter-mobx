69c35c9aa8338b9985f4717b3d6914b6cf72dbaa
Jeedigunta, Giri - Giri <vc.giri.jeedigunta@lowes.com>
2020-01-16 15:46:00 +0530

    Dice button widget extracted

--- lib/main.dart
+++ lib/main.dart
@@ -14,68 +14,68 @@ void main() {
 class DiceExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.amber,
-        appBar: AppBar(
-          backgroundColor: Colors.amberAccent,
-          title: Text(
-            'Tap the dice !!!'.toUpperCase(),
-            style: TextStyle( 
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                fontSize: 16,
-                fontFamily: 'Hind'),
+        home: Scaffold(
+          backgroundColor: Colors.amber,
+          appBar: AppBar(
+            backgroundColor: Colors.amberAccent,
+            title: Text(
+              'Tap the dice !!!'.toUpperCase(),
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontFamily: 'Hind'),
+            ),
+          ),
+          body: SafeArea(
+            child: DiceView(),
           ),
         ),
-        body: SafeArea(
-          child: DiceView(),
-        ),
-      ),
-    );
+      );
 }
 
 class DiceView extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: FlatButton(
-                  child: Observer(
-                    builder: (_) =>
-                        Image.asset('images/dice${diceCounter.left}.png'),
-                  ),
-                  onPressed: diceCounter.roll,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: const <Widget>[
+                DiceButton(dicePosition: 'left'),
+                DiceButton(dicePosition: 'right'),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Observer(
+                builder: (_) => Text(
+                  'Total ${diceCounter.total}',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                      fontSize: 16,
+                      fontFamily: 'Verdana'),
                 ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  child: Observer(
-                    builder: (_) =>
-                        Image.asset('images/dice${diceCounter.right}.png'),
-                  ),
-                  onPressed: diceCounter.roll,
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Observer(
-              builder: (_) => Text(
-                'Total ${diceCounter.total}',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black87,
-                    fontSize: 16,
-                    fontFamily: 'Verdana'),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
+      );
+}
+
+class DiceButton extends StatelessWidget {
+  const DiceButton({this.dicePosition});
+
+  final String dicePosition;
+
+  @override
+  Widget build(BuildContext context) => Expanded(
+      child: FlatButton(
+        child: Observer(
+          builder: (_) => Image.asset('images/dice${dicePosition=='left'?diceCounter.left:diceCounter.right}.png'),
+        ),
+        onPressed: diceCounter.roll,
       ),
     );
 }
